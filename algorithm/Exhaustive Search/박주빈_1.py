a58aac578d9ca4d9e81034c3713b0c54976137d5
Jubin Park <66005132+jbpark0919@users.noreply.github.com>
2024-12-27 19:17:10 +0900

    Create 박주빈_1.py

---
+++ algorithm/Exhaustive Search/박주빈_1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        left, right = 0, len(str_x)-1
+
+        result = True
+
+        while left <= right:
+            if str_x[left] != str_x[right]:
+                return False
+            else:
+                left += 1
+                right -= 1
+        
+        return True
