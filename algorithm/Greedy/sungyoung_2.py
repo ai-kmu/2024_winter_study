1735ec451fc124579ec96dae0b03405c20b75704
Sung-young <smes289@gmail.com>
2025-01-08 02:10:12 +0900

    Add files via upload

---
+++ algorithm/Greedy/sungyoung_2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reach = 0
+        for idx in range(len(nums)):
+            if idx <= reach:
+                reach = max(reach, idx + nums[idx])
+        return len(nums) - 1 <= reach
+        
