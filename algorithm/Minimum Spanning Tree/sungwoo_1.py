33d32e5c6b4ab82bb11477f8e7ac9327d89c2083
Sungwoo-Oh <66308777+Sungwoo-Oh@users.noreply.github.com>
2025-02-05 19:35:47 +0900

    Create sungwoo_1.py

---
+++ algorithm/Minimum Spanning Tree/sungwoo_1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums):
+        if not nums:
+            return 0
+        
+        max_length = 1
+        inc = 1
+        dec = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                inc += 1
+                dec = 1
+            elif nums[i] < nums[i - 1]:
+                dec += 1
+                inc = 1
+            else:
+                inc = 1
+                dec = 1
+            
+            max_length = max(max_length, inc, dec)
+        
+        return max_length
