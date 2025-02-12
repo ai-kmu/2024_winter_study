b3d3dab6dbd45fd486198fc3e1db772a02a252a1
Jubin Park <66005132+jbpark0919@users.noreply.github.com>
2025-02-12 15:03:27 +0900

    Update jubin_2.py

--- algorithm/DP/jubin_2.py
+++ algorithm/DP/jubin_2.py
@@ -1,6 +1,15 @@
-import math
-
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        result = int(math.factorial(n+4)/(math.factorial(n)*24))
-        return result
+        
+        DP = [[0 for _ in range(5)] for _ in range(n+2)]
+
+        for i in range(1, n+2):
+            for j in range(5):
+                if i == 1 or j == 0:
+                    DP[i][j] = 1
+                else:
+                    DP[i][j] = DP[i-1][j] + DP[i][j-1]
+        
+        # print(DP[n+1][4])
+
+        return DP[n+1][4]
